c0b8e599e4b144b087790a6a0e6fef2ce3a5fad7
JosueChorro <josue14149@gmail.com>
2020-05-10 14:39:51 -0600

    He creado una clase con mi nombre

---
+++ src/proyecto_final/HernadezChorroJosueCarlos.java
@@ -0,0 +1,11 @@
+package proyecto_final;
+
+/**
+ *
+ * @author JCH
+ */
+public class HernadezChorroJosueCarlos {
+    public static void main(String[] args) {
+        System.out.println("Soy Josue Carlos Hernandez Chorro");
+    }
+}
